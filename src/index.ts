6300a8282c8185342cd5cd7617527b6d32a1a647
Andrew Branch <andrew@wheream.io>
2018-09-20 00:03:31 -0700

    Write the thing

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './types';
+export * from './mergeProps';
+import { mergeProps } from './mergeProps';
+export default mergeProps;
---
+++ src/mergeProps.test.ts
@@ -0,0 +1,90 @@
+import { mergeProps } from './mergeProps';
+import { MergeableProps } from './types';
+
+describe('mergeProps', () => {
+  test('a single set of props is passed through', () => {
+    const props: MergeableProps = {
+      onClick: () => {},
+      onKeyDown: () => {},
+      className: 'className'
+    };
+
+    expect(mergeProps(props)).toBe(props);
+  });
+
+  test('props of different names are combined', () => {
+    const onClick = () => {};
+    const onFocus = () => {};
+    const onKeyDown = () => {};
+    expect(mergeProps({ onClick }, { onFocus }, { onKeyDown })).toEqual({
+      onClick,
+      onFocus,
+      onKeyDown
+    });
+  });
+
+  test('classNames are concatenated', () => {
+    expect(mergeProps({ className: 'one' }, { className: 'two' })).toEqual({
+      className: 'one two'
+    });
+  });
+
+  test('style props are merged', () => {
+    expect(mergeProps(
+      { style: { display: 'none' } },
+      { style: { color: 'red' } }
+    )).toEqual({
+      style: {
+        display: 'none',
+        color: 'red'
+      }
+    });
+  });
+
+  test('duplicate event handlers are called in sequence', () => {
+    let tape = '';
+    const one = () => tape += 'one ';
+    const two = () => tape += 'two ';
+    const three = () => tape += 'three';
+    mergeProps(
+      { onClick: one },
+      { onClick: two },
+      { onClick: three }
+    ).onClick();
+
+    expect(tape).toBe('one two three');
+  });
+
+  test('arguments are passed to functions', () => {
+    const argsCalled = {
+      one: [null],
+      two: [null]
+    };
+
+    const one = (...args: any[]) => argsCalled.one = args;
+    const two = (...args: any[]) => argsCalled.two = args;
+    const args = [{}, {}];
+    mergeProps(
+      { onClick: one },
+      { onClick: two }
+    ).onClick(...args);
+
+    expect(argsCalled.one[0]).toBe(args[0]);
+    expect(argsCalled.one[1]).toBe(args[1]);
+    expect(argsCalled.two[0]).toBe(args[0]);
+    expect(argsCalled.two[1]).toBe(args[1]);
+  });
+
+  test('first instance of unknown prop is passed through', () => {
+    const unknown = {};
+    expect(mergeProps({ unknown } as any).unknown).toBe(unknown);
+  });
+
+  test('second instance of unknown prop throws an error', () => {
+    const unknown = {};
+    expect(() => mergeProps(
+      { unknown } as any,
+      { unknown } as any)
+    ).toThrow(/unknown/);
+  })
+});
